37f17270cd894015c320a2c6da635cc238ab7a61
horamitama <horahora0828@gmail.com>
2020-12-19 16:56:52 +0900

    add allauth

--- kondate/urls.py
+++ kondate/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls'), name='sign_up'),
     path('', include('kondate_app.urls')),
 ]
