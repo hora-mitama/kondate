f5aecdeeb3e6c9bc63f1519be4042afc2ff98d02
horamitama <horahora0828@gmail.com>
2021-01-12 07:43:46 +0900

    DB設計

--- kondate_app/models.py
+++ kondate_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from accounts.models import CustomUser
 
-# Create your models here.
+
+class Families(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Menus(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Families, on_delete=models.CASCADE)
+    # ingredient = models.CharField(max_length=30)
+    process = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Ingredients(models.Model):
+    menu = models.ForeignKey(Menus, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    amount = models.CharField(max_length=30)
--- kondate_app/views.py
+++ kondate_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class StartView(generic.TemplateView):
     template_name = 'start.html'
 
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.TemplateView, LoginRequiredMixin):
     template_name = 'index.html'
